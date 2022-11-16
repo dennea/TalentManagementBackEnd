2d1a3de4bf115fe62767d75b97fd38d3211b16be
dennea <dennea1228@gmail.com>
2022-11-16 16:46:35 -0700

    done auth controllers

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import AuthRoute from './Routes/AuthRoute.js';
 
-const app = express()
+// Routes
+const app = express(); /* first instance of our server*/
+
+// Middleware
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+
+dotenv.config();
+
+// connect to db
+mongoose
+    .connect(process.env.MONGO_DB,{useNewUrlParser:true, useUnifiedTopology: true})
+    .then(()=>app.listen(process.env.PORT,()=>console.log("Listening")))
+    .catch((error)=> console.log(error));
+
+// usage of routes
+app.use('/auth', AuthRoute);
