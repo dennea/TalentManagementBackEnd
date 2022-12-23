36f131e6a16e5ef0e48d1d657375989c4bd1ad71
dennea <dennea1228@gmail.com>
2022-12-23 14:48:53 -0700

    update user apis

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { deleteUser, followUser, getUser, updateUser, unfollowUser } from "../Controllers/UserController.js";
-
+import { deleteUser, followUser, getUser, updateUser, unfollowUser, getAllUsers } from "../Controllers/UserController.js";
+import authMiddleWare from "../MiddleWare/authMiddleWare.js";
 const router = express.Router();
 
 router.get('/:id',getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow',followUser)
-router.put('/:id/unfollow',unfollowUser)
+router.put('/:id', authMiddleWare, updateUser)
+router.delete('/:id', authMiddleWare, deleteUser)
+router.put('/:id/follow', authMiddleWare, followUser)
+router.put('/:id/unfollow', authMiddleWare, unfollowUser)
+router.get('/', getAllUsers)
 
 export default router;
